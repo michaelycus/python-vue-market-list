cee5c8a9435eb606a326731b30e05285a0940337
Michael Marques <michaelycus@gmail.com>
2020-08-25 07:28:43 -0300

    Add heroku configuration

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,8 +1,8 @@
 """ Production Settings """
 
 import os
+import django_heroku
 import dj_database_url
-from .dev import *
 
 ############
 # DATABASE #
@@ -20,7 +20,10 @@ DATABASES = {
 
 DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
 ALLOWED_HOSTS = ['*']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
